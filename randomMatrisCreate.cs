ca2698e886c8a4e6abb6d6392145719412babad6
Ersin ÇETİN <ersin.cetin@hotmail.com>
2016-06-07 13:12:21 +0300

    ranstgele matris üretme(mxn)

---
+++ randomMatrisCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomMatrisCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            Console.Write("Rastgele olusturulacak matrisin satir sayisini griniz   :"); n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Rastgele olusturulacak matrisin sutun sayisini giriniz  :"); m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[m,n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < m ; i++)
+            {
+                Console.Write("| ");
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = rnd.Next(10, 100);
+                    Console.Write(arr[i,j]+" ");
+                }
+                Console.WriteLine("|");
+            }
+        }
+    }
+}
